c82778b40e23eea367551a656dd8cf4b10e803cc
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2025-02-08 19:31:41 +0530

    Update step03.py

--- step03.py
+++ step03.py
@@ -36,8 +36,8 @@ def execute_query(db_host, db_username, db_password, db_database, query):
           host = os.environ['FTP_SERVER']
           username = os.environ['FTP_USERNAME']
           password = os.environ['FTP_PASSWORD']
-          old_name = 'app-release.apk'
-          new_name = appname
+          old_name = 'downloads/app-release.apk'
+          new_name = 'downloads/'+appname
           
           rename_ftp_file(host, username, password, old_name, new_name)
           while cursor.nextset():
