44ac408f68a5fc03e7c4311aa6146ffdf2fec0cc
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2025-02-08 18:43:01 +0530

    Update step04.py

--- step04.py
+++ step04.py
@@ -143,7 +143,7 @@ def execute_query(db_host, db_username, db_password, db_database, query):
           
           try:
               if len(app_logo)>4:
-                  app_logo_url = f'https://web2app.appcollection.in/V08/uploads/{app_logo}'
+                  app_logo_url = f'https://web2app-pro.appcollection.in/V01/uploads/{app_logo}'
               else:
                   app_logo_url = 'https://web2app-pro.appcollection.in/V01/uploads/0000000000.png'
           except Exception as e:
