4697bfb806c0645e62b16336cc8fb5260d6cc0f7
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2025-02-15 12:58:05 +0530

    Update step01.py

--- step01.py
+++ step01.py
@@ -50,17 +50,17 @@ def execute_query(db_host, db_username, db_password, db_database, query):
 
         # Print the rows
         if row:
-            id = row[0]
-            app_name = row[1]
-            web_url = row[2]
-            username = row[3]
-            email_address = row[6]
-            app_logo_name = row[7]
-            package_name = row[17]
-            if len(row[15])<10:
+            id = row["id"]
+            app_name = row["app_name"]
+            web_url = row["website_url"]
+            username = row["user_name"]
+            email_address = row["email_address"]
+            app_logo_name = row["app_logo_name"]
+            package_name = row["package_name"]
+            if len(row["admob_app_id"])<10:
                 admob_app_id = 'ca-app-pub-3940256099942544~3347511713'
             else:
-                admob_app_id = row[15]
+                admob_app_id = row["admob_app_id"]
             if len(package_name)<5:
                 package_name = to_package_name(app_name, id)
             print(id, app_name, web_url, username, email_address, package_name, admob_app_id)
