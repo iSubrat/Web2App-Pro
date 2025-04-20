18c7391b3165440e6fb6349a3b6a4379539f7f23
GitHub Action <action@github.com>
2025-04-20 16:31:23 +0000

    Update Android Manifest

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,12 +1,12 @@
 //With PHP backend
-const BASE_URL = "https://web2app-pro.appcollection.in/profiles/463";
+const BASE_URL = "https://web2app-pro.appcollection.in/profiles/72";
 
 //without PHP backend
 const PURCHASE_CODE = '';
 const BASE_URL_OFFLINE = 'https://meetmighty.com/mightyweb-public/view/getjson.php?user=$PURCHASE_CODE';
 
 //preference
-const mOneSignalID = "";
+const mOneSignalID = "54c0d639-80c6-4516-8c18-c2b50c1834fc";
 
 //AdmobId
 const adMobBannerId = "YOUR_ADMOB_BANNER_ID";
