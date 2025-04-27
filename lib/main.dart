521b1992e712b816230f0524912545668f5bb71b
Subrat Gupta <85225011+iSubrat@users.noreply.github.com>
2025-04-27 12:48:03 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,96 @@
+// lib/main.dart
+
 import 'dart:async';
 import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import '../AppTheme.dart';
-import '../app_localizations.dart';
-import '../model/LanguageModel.dart';
-import '../screen/DataScreen.dart';
-import '../utils/common.dart';
-import '../utils/constant.dart';
 import 'package:nb_utils/nb_utils.dart';
 import 'package:onesignal_flutter/onesignal_flutter.dart';
+
+import 'AppTheme.dart';
+import 'app_localizations.dart';
+import 'model/LanguageModel.dart';
+import 'screen/DataScreen.dart';
+import 'utils/common.dart';
+import 'utils/constant.dart';
 import 'component/NoInternetConnection.dart';
 import 'store/AppStore.dart';
 
 AppStore appStore = AppStore();
 
-void main() async {
+void main() {
   WidgetsFlutterBinding.ensureInitialized();
   HttpOverrides.global = HttpOverridesSkipCertificate();
-  await initialize();
-  appStore.setDarkMode(aIsDarkMode: getBoolAsync(isDarkModeOnPref));
-  appStore.setLanguage(getStringAsync(APP_LANGUAGE, defaultValue: 'en'));
+  runApp(AppInitializer());
+}
 
-  if (isMobile) {
-    MobileAds.instance.initialize();
-    OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
-    OneSignal.Debug.setAlertLevel(OSLogLevel.none);
-    OneSignal.consentRequired(false);
-    OneSignal.initialize(getStringAsync(ONESINGLE, defaultValue: mOneSignalID));
-    OneSignal.Notifications.requestPermission(true);
-    OneSignal.Notifications.addForegroundWillDisplayListener((event) {
-      print('NOTIFICATION WILL DISPLAY LISTENER CALLED WITH: ${event.notification.jsonRepresentation()}');
-      event.preventDefault();
-      event.notification.display();
-    });
+class AppInitializer extends StatelessWidget {
+  const AppInitializer({Key? key}) : super(key: key);
+
+  Future<void> _initEverything() async {
+    // your custom init (e.g. shared prefs, DB, etc.)
+    await initialize();
+
+    // set up MobX store
+    appStore.setDarkMode(aIsDarkMode: getBoolAsync(isDarkModeOnPref));
+    appStore.setLanguage(getStringAsync(APP_LANGUAGE, defaultValue: 'en'));
+
+    // mobile-only services
+    if (isMobile) {
+      await MobileAds.instance.initialize();
+
+      OneSignal.Debug.setLogLevel(OSLogLevel.verbose);
+      OneSignal.Debug.setAlertLevel(OSLogLevel.none);
+      OneSignal.consentRequired(false);
+
+      OneSignal.initialize(
+        getStringAsync(ONESINGLE, defaultValue: mOneSignalID),
+      );
+      OneSignal.Notifications.requestPermission(true);
+      OneSignal.Notifications.addForegroundWillDisplayListener(
+        (event) {
+          // display even when in foreground
+          event.preventDefault();
+          event.notification.display();
+        },
+      );
+    }
   }
-  runApp(MyApp());
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<void>(
+      future: _initEverything(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState != ConnectionState.done) {
+          // while we’re waiting, show a simple splash
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            theme: AppTheme.lightTheme,
+            darkTheme: AppTheme.darkTheme,
+            themeMode: ThemeMode.light,
+            home: Scaffold(
+              backgroundColor:
+                  AppTheme.lightTheme.scaffoldBackgroundColor,
+              body: const Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        }
+        // once done, launch the real app
+        return const MyApp();
+      },
+    );
+  }
 }
 
 class MyApp extends StatefulWidget {
-  MyApp();
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   _MyAppState createState() => _MyAppState();
@@ -53,23 +102,31 @@ class _MyAppState extends State<MyApp> {
   @override
   void initState() {
     super.initState();
-    setStatusBarColor(appStore.primaryColors, statusBarBrightness: Brightness.light);
-    _connectivitySubscription = Connectivity().onConnectivityChanged.listen((e) async {
-      appStore.setConnectionState(e);
-      if (e == ConnectivityResult.none) {
-        log('not connected');
-        push(NoInternetConnection());
+
+    // status bar color
+    setStatusBarColor(
+      appStore.primaryColors,
+      statusBarBrightness: Brightness.light,
+    );
+
+    // listen for connectivity changes
+    _connectivitySubscription =
+        Connectivity().onConnectivityChanged.listen((result) {
+      appStore.setConnectionState(result);
+      if (result == ConnectivityResult.none) {
+        log('No internet – showing offline screen');
+        push(const NoInternetConnection());
       } else {
+        log('Connected – popping offline screen');
         pop();
-        log('connected');
       }
     });
   }
 
   @override
   void dispose() {
-    super.dispose();
     _connectivitySubscription.cancel();
+    super.dispose();
   }
 
   @override
@@ -77,15 +134,31 @@ class _MyAppState extends State<MyApp> {
     return Observer(builder: (context) {
       return MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: appStore.isNetworkAvailable ? DataScreen() : NoInternetConnection(),
+
+        // decide home based on network
+        home: appStore.isNetworkAvailable
+            ? const DataScreen()
+            : const NoInternetConnection(),
+
+        // localization
         supportedLocales: Language.languagesLocale(),
-        navigatorKey: navigatorKey,
-        localizationsDelegates: [AppLocalizations.delegate, GlobalMaterialLocalizations.delegate, GlobalWidgetsLocalizations.delegate],
-        localeResolutionCallback: (locale, supportedLocales) => locale,
-        locale: Locale(getStringAsync(APP_LANGUAGE, defaultValue: 'en')),
+        localizationsDelegates: const [
+          AppLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+        ],
+        localeResolutionCallback: (locale, locales) => locale,
+        locale: Locale(
+          getStringAsync(APP_LANGUAGE, defaultValue: 'en'),
+        ),
+
+        // theming
         theme: AppTheme.lightTheme,
         darkTheme: AppTheme.darkTheme,
-        themeMode: appStore.isDarkModeOn! ? ThemeMode.dark : ThemeMode.light,
+        themeMode:
+            appStore.isDarkModeOn! ? ThemeMode.dark : ThemeMode.light,
+
+        navigatorKey: navigatorKey,
         scrollBehavior: SBehavior(),
       );
     });
